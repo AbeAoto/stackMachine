d34a2b30d367235a7a80827d7cbd1c9c91ce26ee
阿部碧音 <59000428+AbeAoto@users.noreply.github.com>
2023-05-19 21:45:54 +0900

    initial upload file

---
+++ stackMachine.cpp
@@ -0,0 +1,163 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stack>
+
+class StackMachine
+{
+public:
+  // コンストラクタ
+  StackMachine() {};
+
+  // デストラクタ
+  ~StackMachine() {};
+
+  void parseFile(std::string fileName);
+
+  void push(int num);
+
+  void add();
+
+  void sub();
+
+  void mul();
+
+  void div();
+
+  void print();
+
+  void pop();
+  
+private:
+  std::stack<int> _stack;
+};
+
+void StackMachine::parseFile(std::string fileName)
+{
+  std::ifstream inputFile(fileName);
+
+  // オープンエラー処理
+  if (!inputFile.is_open()) {
+    std::cerr << "Could not open the file : " << fileName << std::endl;
+    exit(1);
+  }
+
+  // 命令分岐(多分あんまりよくない実装)
+  std::string word;
+  while (inputFile >> word) {
+    
+    if (word == "PUSH") {
+      inputFile >> word;
+      push(std::stoi(word));
+    }
+    else if (word == "POP") {
+      pop();
+    }
+    else if (word == "ADD") {
+      add();
+    }
+    else if (word == "SUB") {
+      sub();
+    }
+    else if (word == "MUL") {
+      mul();
+    }
+    else if (word == "DIV") {
+      div();
+    }
+    else if (word == "PRINT") {
+      print();
+    }
+    else {
+      std::cerr << "This operation is not supported!" << std::endl;
+    }
+  }
+
+  inputFile.close();
+}
+
+void StackMachine::push(int num)
+{
+  _stack.push(num);
+}
+
+void StackMachine::add()
+{
+  //もうちょっとちゃんとした例外投げた方がいい
+  if (_stack.size() < 2) {
+    exit(1);
+  }
+
+  int op1 = _stack.top();
+  _stack.pop();
+  int op2 = _stack.top();
+  _stack.pop();
+  _stack.push(op1 + op2);
+}
+
+void StackMachine::sub()
+{
+  //もうちょっとちゃんとした例外投げた方がいい
+  if (_stack.size() < 2) {
+    exit(1);
+  }
+
+  int op1 = _stack.top();
+  _stack.pop();
+  int op2 = _stack.top();
+  _stack.pop();
+  _stack.push(op1 - op2);
+}
+
+void StackMachine::mul()
+{
+  //もうちょっとちゃんとした例外投げた方がいい
+  if (_stack.size() < 2) {
+    exit(1);
+  }
+
+  int op1 = _stack.top();
+  _stack.pop();
+  int op2 = _stack.top();
+  _stack.pop();
+  _stack.push(op1 * op2);
+}
+
+void StackMachine::div()
+{
+  //もうちょっとちゃんとした例外投げた方がいい
+  if (_stack.size() < 2) {
+    exit(1);
+  }
+
+  // (先/後)の順にしたいのでオペランド取得順が逆になっている
+  int op2 = _stack.top();
+  _stack.pop();
+  int op1 = _stack.top();
+  _stack.pop();
+
+  if (op2 == 0) {
+    std::cerr << "[err] divided 0 : " << op1 << "/" << op2 << std::endl;
+    exit(1);
+  }
+
+  _stack.push(op1 / op2);
+}
+
+void StackMachine::print()
+{
+  std::cout << _stack.top() << std::endl;
+}
+
+void StackMachine::pop()
+{
+  _stack.pop();
+}
+
+int main(void)
+{
+  StackMachine sm;
+  sm.parseFile("./TestOperation.txt");
+
+  return 0;
+}
