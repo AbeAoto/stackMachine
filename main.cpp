56cffcafc466e8007e4330c0eb7a725f7e6cb09d
AbeAoto <ce235002@meiji.ac.jp>
2023-05-25 23:21:13 +0900

    テストケースを追加

--- main.cpp
+++ main.cpp
@@ -1,14 +1,23 @@
 ﻿#include "stackMachine.hpp"
-#include "LexicalAnalyzer.hpp"
-#include <utility>
+#include <iostream>
 
 int main(void)
 {
-    LexicalAnalyzer la("./TestOperation copy.txt");
-    std::pair<Token, std::string> p = la.GetToken();
-    std::cout << p.first << " " << p.second << std::endl;
-    p = la.GetToken();
-    std::cout << p.first << " " << p.second << std::endl;
+    StackMachine* sm = new StackMachine();
+
+    std::cout << "ExampleOperationFor.txt start." << std::endl;
+    sm->ParseFile("./ExampleOperationFor.txt");
+    sm->DoInstructions();
+
+    delete sm;
+
+    StackMachine* sm2 = new StackMachine();
+
+    std::cout << "ExampleOperationFunction.txt start." << std::endl;
+    sm2->ParseFile("./ExampleOperationFuntion.txt");
+    sm2->DoInstructions();
+
+    delete sm2;
 
     return 0;
 }
